c59cf8c22d7d100cc8845bc560ec0066a7b33a6f
BleedAngel <aa0918016615@gmail.com>
2026-03-07 21:29:33 +0800

    Update file_type.py

--- Tools/file_type.py
+++ Tools/file_type.py
@@ -3,8 +3,8 @@ import shutil
 
 FILE_TYPE={
     "Image":[".jpg",".png",".jpeg"],
-    "Video":[".mp4",".mov"],
-    "Music":[".mp3",".wav"],
+    "Video":[".mp4",".mov",".mkv"],
+    "Music":[".mp3",".aac",".wav"],
     "Text":[".txt"],
 }
 
@@ -14,6 +14,10 @@ if not os.path.exists(TARGET_DIR):
     exit()
 
 def organize_files():
+
+    count={key:0 for key in FILE_TYPE.keys()}
+    count["Other"]=0
+
     for filename in os.listdir(TARGET_DIR):
         file_path=os.path.join(TARGET_DIR,filename)
 
@@ -23,11 +27,24 @@ def organize_files():
             for folder,extensions in FILE_TYPE.items():
                 if(filename.lower().endswith(tuple(extensions))):
                     dest_folder=os.path.join(TARGET_DIR,folder)
-                    os.makedirs(dest_folder,exist_ok=True)
+                    #os.makedirs(dest_folder,exist_ok=True)
                     print(f"{filename} → {folder}")
                     #shutil.move(file_path,os.path.join(dest_folder,filename))
                     moved=True
+                    count[folder]+=1
                     break
+
+            if(moved==False):
+                dest_folder=os.path.join(TARGET_DIR,"Other")
+                #os.makedirs(dest_folder,exist_ok=True)
+                print(f"{filename} → Other")
+                #shutil.move(file_path,os.path.join(dest_folder,filename))
+                moved=True
+                count["Other"]+=1
+                continue
+
+    summary=" | ".join([f"{key}*{value}"for key,value in count.items() if(value>0)])
+    print(summary)
 
 if __name__=="__main__":
     organize_files()
